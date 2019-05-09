43275c61ef8988325c73ca1a0a6443da46900b60
andreagarz <garciavaldesandrea@gmail.com>
2019-05-09 14:11:48 +0200

    hola pau2

--- data.py
+++ data.py
@@ -7,3 +7,4 @@ from geopy.geocoders import Nominatim
 
 url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
 bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
+jaj
