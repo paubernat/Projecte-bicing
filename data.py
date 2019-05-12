3f4c17505074a44947fce6293ee79b1534c25c12
andreagarz <garciavaldesandrea@gmail.com>
2019-05-12 22:03:25 +0200

    graf medio creado falta mejorarlo mucho mucho esto es un poco chapuza creo

--- data.py
+++ data.py
@@ -6,19 +6,23 @@ from haversine import haversine
 from geopy.geocoders import Nominatim
 
 
-
-
-def stations():
+def create_graph():
     url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
     bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
 
     G = nx.Graph()
     for st in bicing.itertuples():
         G.add_node(st.name, lat=st.lat,lon= st.lon)
+    for node in list(G.node(data=True)):
+        for node2 in list(G.node(data=True)):
+            distance = haversine((node[1]['lon'], node[1]['lat']), (node2[1]['lon'], node2[1]['lat']))
+            if(distance <= 0.600 and distance != 0):
+                if not G.has_edge(node2[0], node[0]):
+                    G.add_edge(node[0], node2[0], distance=distance)
     return G
 
 
-def print_stations(G):
+def print_all(G):
     m = StaticMap(800, 800)
     for node in list(G.node(data=True)):
         marker = CircleMarker((node[1]['lon'], node[1]['lat']), 'red', 8)
@@ -26,4 +30,4 @@ def print_stations(G):
     image = m.render()
     image.save('mapa.png')
 
-print_stations(stations())
+print_all(create_graph())
