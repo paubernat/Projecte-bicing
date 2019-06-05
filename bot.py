6794fa2d6484acfbb47436db6cbd1107eab7d858
Andrea Garcia <50448041+andreagarz@users.noreply.github.com>
2019-06-05 14:41:17 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,36 +1,42 @@
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters
-import data2
+import data
 
-
+#Invalid Command
 def unknown(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Unrecognized command. You will find the valid commands in the /help option")
-
+#Handling errors
 def errorhandler(type, bot, update):
-    #posar error per start y comanda no trobada
-    if type == 1:
-            bot.send_message(chat_id=update.message.chat_id, text="Please start the bot with the command /start")
-    if type == 2:
+    if type == 1: #The user doesn't start the bot
+        bot.send_message(chat_id=update.message.chat_id, text="Please start the bot with the command /start")
+    if type == 2: #There's no graph created
         bot.send_message(chat_id=update.message.chat_id, text="There's no graph, please use the command /graph to create one")
-    if type == 3:
+    if type == 3: #The path doesn't exist
         bot.send_message(chat_id=update.message.chat_id, text="There's no path, maybe the adresses aren't correct")
-    if type == 4:
+    if type == 4: #The user introduces a invalid distance
         bot.send_message(chat_id=update.message.chat_id, text="Please introduce a valid distance")
+    if type == 5: #The user introduces a invalid number of required bikes and docks
+        bot.send_message(chat_id=update.message.chat_id, text="Please introduce a valid number of required bikes and docks")
+    if type == 6: #There's no solution in the distribute command
+        bot.send_message(chat_id=update.message.chat_id, text="Not solution could be found")
 
+#Starts the bot
 def start(bot, update, user_data):
-    user_data['inline'] = 1
     user_data['graph'] = 0
-    start = True
+    user_data['distance'] =0
     bot.send_message(chat_id=update.message.chat_id, text="Hello")
 
+#Gives the author names
 def author(bot, update, user_data):
     bot.send_message(chat_id=update.message.chat_id, text="We're Pau Bernat and Andrea Garcia")
 
+#Help command, explains how the bot works
 def help(bot, update):
     message = ' '
     bot.send_message(chat_id=update.message.chat.id, text=message)
 
+#Creates the graph with the distance introduced by the user
 def graph(bot, update, user_data):
      distance = update.message.text[7:]
      if not distance:
@@ -39,73 +45,106 @@ def graph(bot, update, user_data):
          if not distance.isdigit() or float(distance) <= 0 :
              errorhandler(4, bot, update)
              return
-     graph = data2.create_graph((float(distance))/1000)
+     graph = data.create_graph((float(distance))/1000)
      user_data['graph'] = graph
+     user_data['distance'] = (float(distance))/1000
      bot.send_message(chat_id=update.message.chat_id, text="Graph created")
 
+#Plots the graph create with the /graph command
 def plotgraph(bot, update, user_data):
     g = user_data['graph']
     if g == 0:
         errorhandler(2, bot, update)
     else:
-        map = data2.print_all(g)
+        map = data.print_all(g)
         imatge = map.render()
         imatge.save('mapa.png')
         print('mapa')
         bot.send_photo(chat_id=update.message.chat_id, photo=open('mapa.png', 'rd'))
         os.remove('mapa.png')
 
+#Gives the number of nodes of the graph created by the user
 def nodes(bot, update, user_data):
     g = user_data['graph']
-    if g == 0:s
+    if g == 0:
         errorhandler(2, bot, update)
     else:
-        nodes = data2.number_of_nodes(g)
+        nodes = data.number_of_nodes(g)
         bot.send_message(chat_id=update.message.chat_id, text = nodes)
 
+#Gives the number of edges of the graph created by the user
 def edges(bot, update, user_data):
     g = user_data['graph']
     if g == 0:
         errorhandler(2, bot, update)
     else:
-        edges = data2.number_of_edges(g)
+        edges = data.number_of_edges(g)
         bot.send_message(chat_id=update.message.chat_id, text = edges)
 
+#Gives the number of connex components of the graph created by the user
 def components(bot, update, user_data):
     g = user_data['graph']
     if g == 0:
         errorhandler(2, bot, update)
     else:
-        components = data2.number_of_non_connex_components(g)
+        components = data.number_of_non_connex_components(g)
         bot.send_message(chat_id=update.message.chat_id, text = components)
 
+#Plots the shortest path between two adresses given by the user
 def path(bot, update, user_data):
-    adresses = update.message.text[7:]
     g = user_data['graph']
     if g == 0:
         errorhandler(2, bot, update)
     else:
-        path = data2.shortest_path(g, adresses)
+        adresses = update.message.text[7:]
+        path = data.shortest_path(g, adresses)
         print(path)
         if path == None:
             errorhandler(3, bot, update)
-        map1 = data2.print_path_solo(path)
+        map1 = data.print_path_solo(path)
         imatge = map1.render()
         imatge.save('path_solo.png')
         print('foto')
         bot.send_photo(chat_id=update.message.chat_id, photo=open('path_solo.png', 'rb'))
         print('enviat')
         os.remove('path_solo.png')
-        map2 = data2.print_path_in_graph(path)
+        map2 = data.print_path_in_graph(path)
         imatge2 = map2.render()
         imatge2.save('path_in_graph.png')
         bot.send_photo(chat_id=update.message.chat_id, photo=open('path_in_graph.png', 'rb'))
         os.remove('path_in_graph.png')
 
+#
+def distribute(bot, update, user_data):
+    print('distr')
+    g = user_data['graph']
+    if g == 0:
+        errorhandler(2, bot, update)
+    else:
+        nums = update.message.text[12:]
+        bikes, docks = nums.split(' ')
+        if not bikes.isdigit() or int(bikes) < 0:
+             errorhandler(5, bot, update)
+             return
+        if not docks.isdigit() or int(docks) < 0:
+             errorhandler(5, bot, update)
+             return
+        bikes = int(bikes)
+        docks = int(docks)
+        d = user_data['distance']
+        flow, max = data.distribute(d, bikes, docks)
+        if flow == -1:
+            errorhandler(6, bot, update)
+            return
+        bot.send_message(chat_id=update.message.chat_id, text = flow)
+        bot.send_message(chat_id=update.message.chat_id, text = ("The maximum edge cost is", max[1], "to", max[2], "with", max[0]))
+
+
 TOKEN = open('token.txt').read().strip()
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
+#Commands available
 dispatcher.add_handler(CommandHandler('start', start, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('author', author, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('help', help))
@@ -115,6 +154,7 @@ dispatcher.add_handler(CommandHandler('nodes', nodes, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('edges', edges, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('components', components, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('route', path, pass_user_data=True))
-dispatcher.add_handler(MessageHandler([Filters.command], unknown))
-# engega el bot
+dispatcher.add_handler(CommandHandler('distribute', distribute, pass_user_data=True))
+dispatcher.add_handler(MessageHandler([Filters.command], unknown)) #invalid command
+
 updater.start_polling()
--- data.py
+++ data.py
@@ -1,39 +1,99 @@
+import pandas as pd
 from staticmap import StaticMap, CircleMarker, Line
 import networkx as nx
 from pandas import DataFrame
 from haversine import haversine
 from geopy.geocoders import Nominatim
-import string
+import itertools as it
+from PIL import Image
+
+#haversine (lat,lon)
+#staticmap (lon,lat)
+#ourgraph (lat,lon)
+
+def Bounding_box(G):
+    #Bounding Box
+    L = list(G.node())
+    max_lat, max_lon = L[0]
+    min_lat, min_lon = L[0]
+    for node in L:
+        if node[1] < min_lon:
+            min_lon = node[1]
+        if node[1] > max_lon:
+            max_lon = node[1]
+        if node[0] < min_lat:
+            min_lat = node[0]
+        if node[0] > max_lat:
+            max_lat = node[0]
+
+    coords = ((min_lat, min_lon),(max_lat, min_lon),(max_lat, max_lon),(min_lat, max_lon))
+    return coords
+
+def cross (A,i,j,ii,jj,G,d):
+    for node1 in A[i][j]:
+        for node2 in A[ii][jj]:
+            dist = haversine(node1,node2)
+            if (dist <= d):
+                G.add_edge(node1,node2,weight=dist)
+
+def add_edge_quadrant (G,A,d,lat_rows, long_columns):
+    for i in range (lat_rows):
+        for j in range (long_columns):
+            cross (A,i,j,i,j,G,d)
+            if (i-1>0 and j+1<long_columns):
+                cross (A,i-1,j+1,i,j,G,d)
+            if (j+1<long_columns):
+                cross (A,i,j+1,i,j,G,d)
+            if (i+1<lat_rows and j+1<long_columns):
+                cross (A,i+1,j+1,i,j,G,d)
+            if (i+1<lat_rows):
+                cross (A,i+1,j,i,j,G,d)
+
+def edge_adder (G,d):
+    coord = Bounding_box(G)
+    lat_m, long_m = haversine (coord[0],coord[1]), haversine (coord[1],coord[2])
+    lat_ang, long_ang = coord [1][0] - coord [0][0], coord [2][1]- coord [1][1]
+    dist_ang = (d*lat_ang)/lat_m
+    lat_rows, long_columns = int((lat_ang//dist_ang)+1), int((long_ang//dist_ang)+1)
+    A = [[[] for i in range (long_columns)] for j in range (lat_rows)]
+    for node in list(G.node()):
+        lat,lon = int((node[0]-coord[0][0])//dist_ang),int((node[1]-coord[0][1])//dist_ang)
+        A[lat][lon].append(node)
+
+    add_edge_quadrant (G,A,d,lat_rows, long_columns)
+    print ("edges", number_of_edges(G))
 
 
+#creates the graph with n size as the maximum size of an edge.
 def create_graph(dist):
+    #we using a data base from Barcelona's bicing stations
     url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
     bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
-
     G = nx.Graph()
     for st in bicing.itertuples():
-        G.add_node((st.lon,st.lat))
+        G.add_node((st.lat,st.lon),bikes= 0, docks=0)
 
-    for node in list(G.node()):
-        for node2 in list(G.node()):
-            distance = haversine(node,node2)
-            if (distance <= dist and distance != 0):
-                if (not G.has_edge(node, node2)):
-                    G.add_edge(node, node2, weight=distance)
+    #we add all edges smaller than n.
+    edge_adder(G, dist)
     return G
 
 
+#prints the graph edges and nodes using StaticMap
 def print_all(G):
     m = StaticMap(800, 800)
     for node in list(G.node()):
-        marker = CircleMarker(node, 'black', 5)
+        marker = CircleMarker((node[1],node[0]), 'black', 5)
         m.add_marker(marker)
+
     for edge in list(G.edges()):
-        coords = (edge[0], edge[1])
+        coords = ((edge[0][1],edge[0][0]),(edge[1][1],edge[1][0]))
         line = Line(coords, 'purple', 1)
         m.add_line(line)
+    print('plot')
     return m
 
+
+#transforms adresses to coordinates
 def addressesTOcoordinates(addresses):
     try:
         geolocator = Nominatim(user_agent="bicing_bot")
@@ -44,72 +104,187 @@ def addressesTOcoordinates(addresses):
     except:
         return None
 
-
-def shortest_path(G, adresses):
-    #funcio que la direccio es un node conegut.(g.has_node("dirreccio"))
-
-    coords = addressesTOcoordinates(adresses)
-    if coords == None:
-        return None
-
-    coords1, coords2 = coords
-
-    if (not G.has_node(coords1)):
-        G.add_node(coords1)
-        for node in list(G.node()):
-            distance = haversine (node,coords1)
-            if (distance!=0):
-                G.add_edge(node, coords1, weight= distance*2.5)
-    else:
-        for node in list(G.node()):
-            if (not G.has_edge(node, coords1)):
-                distance = haversine (node,coords1)
-                if (distance!=0):
-                    G.add_edge (node, coords1, weight= distance*2.5)
-
-
-    if (not G.has_node(coords2)):
-        G.add_node(coords2)
-        for node in list(G.node()):
-            distance = haversine (node,coords2)
-            if (distance!=0):
-                G.add_edge(node, coords2, weight= distance*2.5)
-    else:
-        for node in list(G.node()):
-            if (not G.has_edge(node, coords2)):
-                distance = haversine (node,coords2)
-                if (distance!=0):
-                    G.add_edge (node, coords2, weight= distance*2.5)
-
+def complete_known_edge (G, coords):
     for node in list(G.node()):
-        print (node)
+        if (not G.has_edge(node, coords)):
+            distance = haversine (node,coords)
+            if (distance!=0):
+                G.add_edge (node, coords, weight= distance*2.5)
 
-    '''
-    print ("nodes")
-    print ()
-    for node in  list (G.node()):
-        print (node)
+#matches a new node with every single node
+def complete_new_edge (G,coords):
+    G.add_node(coords)
+    for node in list(G.node()):
+        distance = haversine (node,coords)
+        if (distance!=0):
+            G.add_edge(node, coords, weight= distance*2.5)
 
-    print ("edges")
-    print()
-    for edge in list (G.edges()):
-        print (edge, edge.weight())
-    '''
-    lenght, path = nx.bidirectional_dijkstra(G,coords1,coords2)
-    print (path)
-    print (lenght)
+#matches an existent node with every single node
+def print_path_in_graph (G, path):
 
     m = StaticMap(800, 800)
 
-    for node in path:
+    for node in list(G.node()):
+        marker = CircleMarker((node[1],node[0]), 'black', 5)
+        m.add_marker(marker)
 
-        marker = CircleMarker((node[1],node[0]), 'red', 8)
+    for edge in list(G.edges()):
+        coords = ((edge[0][1],edge[0][0]),(edge[1][1],edge[1][0]))
+        line = Line(coords, 'grey', 1)
+        m.add_line(line)
+
+    for node in path:
+        marker = CircleMarker((node[1],node[0]), 'blue', 3)
         m.add_marker(marker)
 
     for i in range (len(path)-1):
         coords = ((path[i][1],path[i][0]),(path[i+1][1],path[i+1][0]))
-        line = Line(coords, 'green', 9)
+        line = Line(coords, 'blue', 2)
         m.add_line(line)
+    return m
 
-    image = m.render()
-    image.save('path.png')
+#prints the smallest path between 2 points
+def print_path_solo (path):
+
+    m = StaticMap(800,800)
+
+    for node in path:
+        marker = CircleMarker((node[1],node[0]), 'blue', 3)
+        m.add_marker(marker)
+
+    for i in range (len(path)-1):
+        coords = ((path[i][1],path[i][0]),(path[i+1][1],path[i+1][0]))
+        line = Line(coords, 'blue', 2)
+        m.add_line(line)
+    return m
+
+#prints the smallest distance in time between 2 points.
+def shortest_path(G, adresses):
+    #funcio que la direccio es un node conegut.(g.has_node("dirreccio"))
+    coords = addressesTOcoordinates(adresses)
+    print(coords)
+    if coords == None:
+        return None
+    coords1, coords2 = coords
+
+    #afegim els punts de origen i de arribada com a nodes.
+    #per a evitar operacions innecessàries, la velocitat bici es va a 10km/h i caminant a 4km/h,
+    #sabem que es trigarà 2,5 vegades més anant caminant, per tant, multipliquem la distancia de
+    #trobar el shortest_path serà el mateix
+    if (not G.has_node(coords1)):
+        complete_new_edge(G,coords1)
+    else:
+        complete_known_edge(G,cords1)
+
+    if (not G.has_node(coords2)):
+        complete_new_edge(G,coords2)
+    else:
+        complete_known_edge(G,cords2)
+
+    lenght, path = nx.bidirectional_dijkstra(G,coords1,coords2)
+
+    G.remove_node(coords1)
+    G.remove_node(coords2)
+    return path
+
+
+def number_of_non_connex_components (G):
+    return nx.number_connected_components(G)
+
+def number_of_nodes (G):
+    return len(G)
+
+def number_of_edges (G):
+    return nx.number_of_edges(G)
+
+def distribute (radius, requiredBikes, requiredDocks):
+    url_info = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
+    url_status = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_status'
+    stations = DataFrame.from_records(pd.read_json(url_info)['data']['stations'], index='station_id')
+    bikes = DataFrame.from_records(pd.read_json(url_status)['data']['stations'], index='station_id')
+    nbikes = 'num_bikes_available'
+    ndocks = 'num_docks_available'
+    bikes = bikes[[nbikes, ndocks]] # We only select the interesting columns
+    TotalBikes = bikes[nbikes].sum()
+    TotalDocks = bikes[ndocks].sum()
+
+    G = nx.DiGraph()
+    G.add_node('TOP') # The green node
+    demand = 0
+
+    for st in bikes.itertuples():
+        idx = st.Index
+        if idx not in stations.index: continue  # Salta a la seguent iteracio
+        idx = st.Index
+        stridx = str(idx)
+
+        # The blue (s), black (g) and red (t) nodes of the graph
+        s_idx, g_idx, t_idx = 's'+stridx, 'g'+stridx, 't'+stridx
+        G.add_node(g_idx)
+        G.add_node(s_idx)
+        G.add_node(t_idx)
+
+        b, d = st.num_bikes_available, st.num_docks_available
+        req_bikes = max(0, requiredBikes - b)
+        req_docks = max(0, requiredDocks - d)
+
+        # Some of the following edges require attributes
+        G.add_edge('TOP', s_idx)
+        G.add_edge(s_idx, g_idx, capacity = max(0, b-requiredBikes))
+        G.add_edge(g_idx, t_idx, capacity = max(0, d-requiredDocks))
+        G.add_edge(t_idx, 'TOP')
+
+        if req_bikes > 0:
+            demand += req_bikes
+            G.nodes[t_idx]['demand'] = req_bikes
+        elif req_docks > 0:
+            demand -= req_docks
+            G.nodes[s_idx]['demand'] = -req_docks
+            # something else must be done here (demand?)
+
+    G.nodes['TOP']['demand'] = -demand
+
+    for idx1, idx2 in it.combinations(stations.index.values, 2):
+        coord1 = (stations.at[idx1, 'lat'], stations.at[idx1, 'lon'])
+        coord2 = (stations.at[idx2, 'lat'], stations.at[idx2, 'lon'])
+        dist = haversine(coord1, coord2)
+        if dist <= radius:
+            dist = int(dist*1000)
+            # The edges must be bidirectional: g_idx1 <--> g_idx2
+            G.add_edge('g'+str(idx1), 'g'+str(idx2), weight=dist)
+            G.add_edge('g'+str(idx2), 'g'+str(idx1), weight=dist)
+
+    err = False
+
+    try:
+        flowCost, flowDict = nx.network_simplex(G)
+
+    except nx.NetworkXUnfeasible:
+        err = True
+        print("No solution could be found")
+
+    except:
+        err = True
+        print("***************************************")
+        print("*** Fatal error: Incorrect graph model ")
+        print("***************************************")
+
+    if not err:
+        c = ("The total cost of transfering bikes is", flowCost/1000, "km")
+
+    # We update the status of the stations according to the calculated transportation of bicycles
+    disb = []
+    for src in flowDict:
+        if src[0] != 'g': continue
+        idx_src = int(src[1:])
+        for dst, b in flowDict[src].items():
+            if dst[0] == 'g' and b > 0:
+                idx_dst = int(dst[1:])
+                dis = [(G.edges[src, dst]['weight'])*b, idx_src, idx_dst]
+                disb.append(dis)
+                bikes.at[idx_src, nbikes] -= b
+                bikes.at[idx_dst, nbikes] += b
+                bikes.at[idx_src, ndocks] += b
+                bikes.at[idx_dst, ndocks] -= b
+
+    return c, max(disb)
